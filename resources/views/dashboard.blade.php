3ad83b5af4fd5c383887787933186adfc0d26bd3
HexPang <hexpang@jabstruse.com>
2016-08-25 17:29:49 +0800

    Dashboard template update.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,12 +16,24 @@
                 <ul class="nav of nav-stacked">
                     <li class="tq">Dashboards</li>
                     <li class="active">
-                        <a href="/dashboard/">{{ app('translator')->trans('language.text.dashboard') }}</a>
+                        <a href="/application/list">Applications</a>
                     </li>
                 </ul>
                 <hr class="rw aky">
             </div>
         </nav>
     </div>
-    @yield('body')
+
+    <div class="hc aps">
+        <div class="apa">
+            <div class="apb">
+                <h6 class="apd">Dashboards</h6>
+                <h2 class="apc">{{ app('translator')->trans('language.text.dashboard') }} - {{ $user->name }}</h2>
+            </div>
+        </div>
+        <hr class="aky">
+
+        @yield('body')
+    </div>
+
 @endsection
