7c212347cdc95e1dc06c64624891bfc1d09b72ce
HexPang <hexpang@jabstruse.com>
2016-08-25 19:47:46 +0800

    Bug Fixed.

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -12,20 +12,21 @@ class APIController extends Controller
 {
   public function GetKey(Request $request,$app_id,$key){
     $data = CloudData::where('app_id',$app_id)->where('key',$key)->first();
-    return Response::json(1,$data)->data;
+    return Response::json(1,$data);
   }
   public function Process(Request $request,$app_id,$key){
       $data = CloudData::where('app_id',$app_id)->where('key',$key)->first();
       if($data) {
           //更新
-          $data->data = $request->get('data');
+          $item = urldecode($request->get('data'));
+          $data->data = $item;
           $data->save();
       }else{
           //插入
           $data = CloudData::create([
               'app_id' => $app_id,
               'key' => $key,
-              'data' => $request->get('data')
+              'data' => urldecode($request->get('data'))
           ]);
       }
       return Response::json(1,$data);
