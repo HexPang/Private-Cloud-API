3c821473b9ef499c49dc497834b4c0cde40ae240
HexPang <hexpang@jabstruse.com>
2016-08-25 17:29:58 +0800

    Index Template update.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,1 @@
-@extends('dashboard')
-
-@section('body')
-<div class="hc aps">
-    <div class="apa">
-        <div class="apb">
-            <h6 class="apd">Dashboards</h6>
-            <h2 class="apc">{{ app('translator')->trans('language.text.dashboard') }}</h2>
-        </div>
-    </div>
-    <hr class="aky">
-
-    </div>
-</div>
-
-@endsection
+@extends('application.list')
