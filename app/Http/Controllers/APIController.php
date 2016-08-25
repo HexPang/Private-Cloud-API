fbf0fea8983400f0c13657d2d2d3dc49ec3734ba
HexPang <hexpang@jabstruse.com>
2016-08-25 15:21:39 +0800

    Add GetKey method.

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -10,20 +10,24 @@ use Response;
 
 class APIController extends Controller
 {
-    public function Process(Request $request,$app_id,$key){
-        $data = CloudData::where('app_id',$app_id)->where('key',$key)->first();
-        if($data) {
-            //更新
-            $data->value = $request->get('data');
-            $data->save();
-        }else{
-            //插入
-            $data = CloudData::create([
-                'app_id' => $app_id,
-                'key' => $key,
-                'value' => $request->get('data')
-            ]);
-        }
-        return Response::json(1,$data);
-    }
+  public function GetKey(Request $request,$app_id,$key){
+    $data = CloudData::where('app_id',$app_id)->where('key',$key)->first();
+    return Response::json(1,$data);
+  }
+  public function Process(Request $request,$app_id,$key){
+      $data = CloudData::where('app_id',$app_id)->where('key',$key)->first();
+      if($data) {
+          //更新
+          $data->value = $request->get('data');
+          $data->save();
+      }else{
+          //插入
+          $data = CloudData::create([
+              'app_id' => $app_id,
+              'key' => $key,
+              'value' => $request->get('data')
+          ]);
+      }
+      return Response::json(1,$data);
+  }
 }
